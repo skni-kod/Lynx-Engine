e1188e2b11fd2db5c600c54091f907102214e04c
Kacper Walicki <eupheduch@gmail.com>
2021-12-05 13:44:18 +0100

    Split CMake into multiple files

---
+++ cmake/platform_sources.cmake
@@ -0,0 +1,9 @@
+SET(PLATFORM_SOURCES
+        Window.cpp
+        # Clipboard.cpp
+        # Jakieś
+        # inne
+        # pliki
+        # zależne od platformy
+        # idą tutaj.cpp
+        )
